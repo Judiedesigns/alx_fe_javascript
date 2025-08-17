fdf02564ddde177d5326ccd9d0433b2aded1a080
Florence <florencekey22@gmail.com>
2025-08-17 10:59:40 +0100

    dom manipulation

---
+++ dom-manipulation/script.js
@@ -0,0 +1,31 @@
+let quotes = [
+  { text: "The best way to get started is to quit talking and begin doing.", category: "Motivation" },
+  { text: "Your limitation—it’s only your imagination.", category: "Inspiration" },
+  { text: "Push yourself, because no one else is going to do it for you.", category: "Motivation" }
+];
+
+function showRandomQuote() {
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  let randomQuote = quotes[randomIndex];
+  let quoteDisplay = document.getElementById("quoteDisplay");
+  quoteDisplay.textContent = `"${randomQuote.text}" — (${randomQuote.category})`;
+}
+
+document.getElementById("newQuote").addEventListener("click", showRandomQuote);
+
+function addQuote() {
+  let newText = document.getElementById("newQuoteText").value;
+  let newCategory = document.getElementById("newQuoteCategory").value;
+
+  if (newText === "" || newCategory === "") {
+    alert("Please enter both a quote and a category!");
+    return;
+  }
+
+  quotes.push({ text: newText, category: newCategory });
+
+  document.getElementById("newQuoteText").value = "";
+  document.getElementById("newQuoteCategory").value = "";
+
+  alert("New quote added successfully!");
+}
